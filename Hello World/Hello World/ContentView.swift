acd7ebc9731b5fac1d901d6e0f15488b333ce244
mahmoudthabit <54415304+mahmoudthabit@users.noreply.github.com>
2020-09-03 23:28:19 -0700

    changed

--- Hello World/Hello World/ContentView.swift
+++ Hello World/Hello World/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Hello, World! changeds")
     }
-}
+}s
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

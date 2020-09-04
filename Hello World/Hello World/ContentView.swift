4ef9672241e99d542b6abc69b247053362d6c43a
mahmoudthabit <54415304+mahmoudthabit@users.noreply.github.com>
2020-09-03 23:31:52 -0700

    removed the 's'

--- Hello World/Hello World/ContentView.swift
+++ Hello World/Hello World/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, World! changeds")
     }
-}s
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

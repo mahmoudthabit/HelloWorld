f12831c0a9933f6c05d76ac8921910ff7fe2fbd7
mahmoudthabit <54415304+mahmoudthabit@users.noreply.github.com>
2020-09-03 23:16:28 -0700

    created an initial Xcod Project

---
+++ Hello World/Hello World/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello World
+//
+//  Created by mlt446 on 9/3/20.
+//  Copyright © 2020 mlt446. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
